ce30c0f410cd53011829e756585f689d27e0d799
Komii <chinowlepogi@gmail.com>
2026-03-23 16:53:27 +0800

    lastsheet

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,74 @@
-import React from 'react'
+import Input from "../componets/form/Input";
+import MainLayout from "../layouts/MainLayout";
+import Card from "../componets/Card";
+import { supabase } from "../Utils/SuperBase";
+import SendIcon from "../componets/icons/SendIcon";
+import { useContext, useEffect } from "react";
+import { SessionContext } from "../contexts/SessionContext";
+import { useNavigate } from "react-router";
 
-const login = () => {
-  return (
-    <div>
-      <MainLayout>
-			<div className="min-h-screen flex flex-col">
-				<div className="flex justify-center items-center flex-1">
-					<Card>
-						<h1 className="text-xl font-bold">Sign Up</h1>
-						<form onSubmit={handleSubmit}>
-							<Input
-								name="firstname"
-								placeholder="Enter your First Name"
-								label="Firstname"
-								type="text"
-							/>
-							<Input
-								name="lastname"
-								placeholder="Enter your Last Name"
-								label="Lastname"
-								type="text"
-							/>
-							<Input
-								name="email"
-								placeholder="Enter your Email"
-								label="Email"
-								type="email"
-							/>
-							<Input
-								name="password"
-								placeholder="Enter your Password"
-								label="Password"
-								type="password"
-							/>
-							<button className="btn btn-primary rounded-full mt-5">
-								<SendIcon className="text-sm" /> Submit
-							</button>
-						</form>
-					</Card>
-				</div>
-			</div>
-		</MainLayout>
-    </div>
-  )
+const Login = () => {
+    const session = useContext(SessionContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (session) {
+            navigate("/");
+        }
+    }, [session, navigate]);
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const LoginForm = {
+            email: formData.get("email"),
+            password: formData.get("password"),
+        };
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: LoginForm.email,
+            password: LoginForm.password,
+        });
+
+        if (error) alert(error);
+
+        if (data) console.log(data);
+    };
+
+    useEffect(() => {
+        
+    }, []);
+
+
+    return (
+        <MainLayout>
+            <div className="min-h-screen flex flex-col">
+                <div className="flex justify-center items-center flex-1">
+                    <Card>
+                        <h1 className="text-xl font-bold">Login</h1>
+                        <form onSubmit={handleSubmit}>
+
+                            <Input
+                                name="email"
+                                placeholder="Enter your Email"
+                                label="Email"
+                                type="email"
+                            />
+                            <Input
+                                name="password"
+                                placeholder="Enter your Password"
+                                label="Password"
+                                type="password"
+                            />
+                            <button className="btn btn-primary rounded-full mt-5">
+                                <SendIcon className="text-sm" /> Submit
+                            </button>
+                        </form>
+                    </Card>
+                </div>
+            </div>
+        </MainLayout>
+    )
 }
 
-export default login
+export default Login;
